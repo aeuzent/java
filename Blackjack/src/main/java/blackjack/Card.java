bdef14e4e67dca2b919d999ba26a16fa4926eb21
aeuzent <aeuzent@gmail.com>
2015-08-20 11:52:38 -0400

    Made some changes to card

--- Blackjack/src/main/java/blackjack/Card.java
+++ Blackjack/src/main/java/blackjack/Card.java
@@ -14,17 +14,38 @@ public class Card{
 	private String cardName;
 	private static final String[] rankNames = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
 	private static final String[] suitNames = {"Hearts", "Diamonds", "Clubs", "Spades"};
-	
-	
+
+	public static final int HEARTS = 0;
+	public static final int DIAMONDS = 1;
+	public static final int CLUBS = 2;
+	public static final int SPADES = 3;
+
+    public static final int ACE = 0;
+	public static final int TWO = 1;
+	public static final int THREE = 2;
+	public static final int FOUR = 3;
+	public static final int FIVE = 4;
+	public static final int SIX = 5;
+	public static final int SEVEN = 6;
+	public static final int EIGHT = 7;
+	public static final int NINE = 8;
+	public static final int TEN = 9;
+	public static final int JACK = 10;
+    public static final int QUEEN = 11;
+    public static final int KING = 12;
+
 	/**
 	 * Constructor for Card
 	 * @param in
 	 */
-	public Card(int in) {
-		suit = in / 13;
-		rank = in % 13;
-
-		cardName = rankNames[rank] + " of " + suitNames[suit];
+	public Card(int in) throws IllegalArgumentException{
+        if(in>=0 && in < 52) {
+            suit = in / 13;
+            rank = in % 13;
+            cardName = rankNames[rank] + " of " + suitNames[suit];
+        } else {
+            throw new IllegalArgumentException();
+        }
 	}
 	
 	
--- Blackjack/src/test/java/blackjack/CardTest.java
+++ Blackjack/src/test/java/blackjack/CardTest.java
@@ -12,16 +12,41 @@ public class CardTest extends TestCase {
 
     @Test
     public void testGetRank() throws Exception {
-
+        Card c = new Card(24);
+        assertEquals("Expected rank = 11, got " + c.getRank(),Card.QUEEN,c.getRank());
     }
 
     @Test
     public void testGetSuit() throws Exception {
-
+        Card c = new Card(15);
+        assertEquals("Expected suit = 11, got " + c.getSuit(), Card.DIAMONDS, c.getSuit());
     }
 
     @Test
     public void testToString() throws Exception {
+        Card c = new Card(48);
+        assertTrue("Expected \"Ten of Spades\", got " + c.toString(), c.toString().equals("Ten of Spades"));
+    }
 
+    @Test
+    public void testInvalidHiCards() throws Exception {
+        boolean thrown = false;
+        try {
+            Card c = new Card(53);
+        } catch (IllegalArgumentException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+
+    @Test
+    public void testInvalidLoCards() throws Exception {
+        boolean thrown = false;
+        try {
+            Card c = new Card(-2);
+        } catch (IllegalArgumentException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
     }
 }
